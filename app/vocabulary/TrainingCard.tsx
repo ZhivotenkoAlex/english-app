2dda7ca1db619a0516305f5537ee0bb20d02761b
Marina Sheigets <marinasejgec34@gmail.com>
2024-01-10 02:29:01 +0200

    feat: create basic structure

---
+++ app/vocabulary/TrainingCard.tsx
@@ -0,0 +1,56 @@
+import { renderElement } from '@/helpers/renderElement'
+import { ITraining } from '@/types'
+import Image from 'next/image'
+import React from 'react'
+import styled from 'styled-components'
+
+interface ITrainingCard {
+  training: ITraining
+}
+function TrainingCard({ training }: ITrainingCard) {
+  return (
+    <Wrapper $gridArea={training.gridArea}>
+      <StyledImage src={training.background} alt={training.title} />
+      <TextBlock>
+        <Title $titleColor={training.titleColor}>{training.title}</Title>
+        <Subtitle>{training.subtitle}</Subtitle>
+      </TextBlock>
+    </Wrapper>
+  )
+}
+
+const StyledImage = styled(Image)`
+  border-radius: 10px;
+  height: 100%;
+  width: 100%;
+`
+
+const TextBlock = styled.div`
+  position: absolute;
+  top: 2rem;
+  left: 2rem;
+`
+const Title = styled.p<{ $titleColor: string }>`
+  font-size: 1.5rem;
+  padding-bottom: 0.5rem;
+  color: ${({ $titleColor }) => $titleColor};
+`
+const Subtitle = styled.p`
+  color: rgba(0, 0, 0, 0.3);
+`
+const Wrapper = styled.div<{ $gridArea: string }>`
+  cursor: pointer;
+  position: relative;
+  border-radius: 10px;
+  box-shadow:
+    rgba(50, 50, 93, 0.25) 0px 6px 12px -2px,
+    rgba(0, 0, 0, 0.3) 0px 3px 7px -3px;
+
+  grid-area: ${({ $gridArea }) => $gridArea};
+
+  &:hover {
+    transform: scale(1.02);
+  }
+`
+
+export default TrainingCard
