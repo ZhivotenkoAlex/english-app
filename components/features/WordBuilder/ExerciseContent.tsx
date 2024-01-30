51ceede522b782e6fb23dd58c83198d103362077
Marina Sheigets <marinasejgec34@gmail.com>
2024-01-30 19:03:09 +0200

    fix: add media queries; add bigger delay for incorrect click

--- components/features/WordBuilder/ExerciseContent.tsx
+++ components/features/WordBuilder/ExerciseContent.tsx
@@ -48,7 +48,7 @@ function ExerciseContent({
           updatedChars[index].background = themeColors.primary
           return updatedChars
         })
-      })
+      }, 500)
     } else {
       setInitialChars(initialChars.filter((el, i) => i !== index))
       setContainedCells([...containedCells, char])
@@ -136,6 +136,9 @@ const Wrapper = styled.div`
   flex-direction: column;
   align-items: center;
   gap: 1rem;
+  @media screen and (max-width: 480px) {
+    padding: 1.5rem;
+  }
 `
 
 export default ExerciseContent
